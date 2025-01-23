b41781c66236b9d19aee3a7baee51d6ad0fc1105
rodri <rodri@192.168.0.179>
2025-01-22 20:35:24 -0500

    Proyecto Final Corrales

---
+++ src/trabajofinal/TRABAJOFINAL.java
@@ -0,0 +1,38 @@
+package trabajofinal;
+
+import java.util.Scanner;
+
+public class TRABAJOFINAL {
+    public static void main(String args[]) {
+        int vt1, vt2, vt3;
+        double sm, pt, c;
+        String vendedor;
+
+        Scanner lectura = new Scanner(System.in);
+
+        System.out.print("Nombre del Vendedor: ");
+        vendedor = lectura.next();
+        System.out.print("Sueldo Mensual: ");
+        sm = lectura.nextDouble();
+        System.out.print("Venta 1: ");
+        vt1 = lectura.nextInt();
+        System.out.print("Venta 2: ");
+        vt2 = lectura.nextInt();
+        System.out.print("Venta 3: ");
+        vt3 = lectura.nextInt();
+
+        // Cálculos
+        c = (vt1 + vt2 + vt3) * 0.10; // Calcular comisión (corrección de vt14 a vt1)
+        pt = sm + c; // Sueldo total con comisión
+
+        // Resultados
+        System.out.println("Resumen:");
+        System.out.println("Vendedor: " + vendedor);
+        System.out.println("Sueldo Mensual: " + sm);
+        System.out.println("Ventas Totales: " + (vt1 + vt2 + vt3));
+        System.out.println("Comisión: " + c);
+        System.out.println("Sueldo Total: " + pt);
+
+        lectura.close();
+    }
+}
